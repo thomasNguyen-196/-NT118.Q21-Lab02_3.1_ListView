eb1b77feae81bae6a24d9adfa7ca8fc8179e1041
thomasNguyen-196 <thomasspielberg5@gmail.com>
2026-03-31 20:41:22 +0700

    Remove commented-out onCreate boilerplate code in MainActivity.

--- app/src/main/java/com/example/lab02_31_listview/MainActivity.java
+++ app/src/main/java/com/example/lab02_31_listview/MainActivity.java
@@ -14,19 +14,6 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        EdgeToEdge.enable(this);
-//        setContentView(R.layout.activity_main);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
-//    }
-
     private void initiateListViewContainer() {
         // Bài 3.1
         ListView lvPerson = findViewById(R.id.lv_person);
